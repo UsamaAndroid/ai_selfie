f9b3e5d63a84890b044816377ed21997c25b43ee
Usama Tariq <56360764+UsamaAndroid@users.noreply.github.com>
2025-07-07 15:02:59 +0800

    a
    a

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,12 @@
+// app/api/uploadthing/core.ts
+import { createUploadthing, type FileRouter } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "16MB" } }).onUploadComplete(({ file }) => {
+    return { url: file.url };
+  }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
